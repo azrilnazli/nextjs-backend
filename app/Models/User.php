7a831305357ba6fac09158c7c4c7ca7dd14a20b1
Azril Nazli Alias <sales@stream.my>
2023-10-03 21:19:24 +0800

    Login

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Modules\User\Models;
+namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
--- modules/User/Http/Controllers/AppController.php
+++ modules/User/Http/Controllers/AppController.php
@@ -4,7 +4,7 @@ namespace Modules\User\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
-use Modules\User\Models\User;
+use App\Models\User;
 
 class AppController extends BaseController
 {
@@ -12,7 +12,7 @@ class AppController extends BaseController
     {
         $user = User::find(1); // test user
         $token = $user->createToken('token-name')->plainTextToken;
-        return response()->json(['token' => $token]); 
+        return response()->json(['token' => $token]);
     }
 
     function user()
@@ -20,8 +20,19 @@ class AppController extends BaseController
         return response()->json(['message' => 'This is protected resource'], 200);
     }
 
-    public function login(){
-        return response()->json(['message' => 'Please login'], 200);
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) { // if login is success
+            $user = Auth::user();
+            $token = $user->createToken('authToken')->plainTextToken;
+            // return success 200 with token
+            return response()->json(['message' => 'Login successful', 'token' => $token], 200);
+        }
+
+        // return unauthorized 401
+        return response()->json(['message' => 'Invalid credentials'], 401);
     }
 
     public function logout(Request $request)
